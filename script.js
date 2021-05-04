256d631658d98ca5bb878aa96a4e99711b176918
MuhammadWasif <wasif33@outlook.com>
2021-05-04 14:49:24 +0500

    minor fixes

--- script.js
+++ script.js
@@ -2,26 +2,25 @@ function compute() {
   var principal = document.getElementById('principal').value;
   var rate = document.getElementById('rate').value;
   var years = document.getElementById('years').value;
-
+  if (principal == 0) {
+    alert('Enter a positive number');
+  }
   var amount = (principal * years * rate) / 100;
   var year = new Date().getFullYear() + parseInt(years);
 
-  if (principal === 0) {
-    alert('Enter a positive number');
-  }
   document.getElementById('result').innerHTML =
-    'If you deposit ' +
+    'If you deposit <mark>' +
     principal +
-    ',<br>at an interest rate of ' +
+    '</mark>,<br>at an interest rate of <mark>' +
     rate +
-    '%<br>You will receive an amount of ' +
+    '%</mark><br>You will receive an amount of <mark>' +
     amount +
-    ',<br>in the year ' +
+    '</mark>,<br>in the year <mark>' +
     year +
-    '<br>';
+    '</mark><br>';
 }
 
 function updateRate() {
   var rateval = document.getElementById('rate').value;
-  document.getElementById('rate_val').innerText = rateval;
+  document.getElementById('rate_val').innerText = rateval + '%';
 }
